f45a925b2b65dd2b3ad01ddac0cda7326c9c1360
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-11 10:26:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+data "intersight_organization_organization" "organization" {
+  name = "default"
+}
 
+resource "intersight_kubernetes_sys_config_policy" "test123" {
+
+  name = "test_k8s_sysconfig"
+
+  dns_servers = ["10.51.75.254"]
+
+  ntp_servers = ["10.51.75.254"]
+  timezone = "Europe/Vienna"
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.organization.moid
+  }
+}
