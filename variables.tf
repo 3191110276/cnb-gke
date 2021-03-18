3b0ac55583ebc6ace8ea91b72479c264615b54f4
namjachingu <felicia.rahim@gmail.com>
2021-03-18 09:55:23 +0100

    variables file

---
+++ variables.tf
@@ -0,0 +1,39 @@
+/*  
+variable "apikey" {
+  type    = string
+}
+
+variable "secretkey" {
+  type    = string
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.18.12"
+}
+ */
+ 
+variable "project" {
+  description = "The project ID to host the cluster in."
+  type = string
+}
+
+variable "region" {
+  type = string
+  #default = "europe-west3"
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  description = "This authenticates the google provider."
+type = string
+}
+
+variables "zone" {
+  type = string
+}
+
+#variable "name" {
+#  description = "Name of the cluster"
+#  type = string
+#}
+
