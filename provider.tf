4ce494a142d95b18b741e84cb06148e493c8105c
namjachingu <felicia.rahim@gmail.com>
2021-03-18 09:54:11 +0100

    provider file

---
+++ provider.tf
@@ -0,0 +1,23 @@
+#terraform {
+#  required_providers {
+#    intersight = {
+#      source = "CiscoDevNet/intersight"
+#     version = "1.0.0"
+#    }
+#  }
+#}
+
+#provider "intersight" {
+#  apikey = var.apikey
+#  secretkey = var.secretkey
+#  endpoint = "intersight.com"
+#}
+
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
+  project     = var.project 
+  region      = var.region
+  zone        = var.zone
+}
+
+
