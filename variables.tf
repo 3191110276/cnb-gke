3832bdcbb5bdb60c22201ec1d4afa93cd0b20c1c
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-03-19 13:10:01 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,22 @@ variable "k8s_version" {
   type    = string
   default = "1.18.12"
 }
+
+variable "project" {
+  description = "The project ID to host the cluster in."
+  type = string
+}
+
+variable "region" {
+  type = string
+  #default = "europe-west3"
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  description = "This authenticates the google provider."
+type = string
+}
+
+variables "zone" {
+  type = string
+}
