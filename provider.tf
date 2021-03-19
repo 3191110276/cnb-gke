35dd452da4a730cb9346b33f1985dfb3d95c20e9
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-03-19 13:27:44 +0100

    Updated vars

--- provider.tf
+++ provider.tf
@@ -1,25 +1,10 @@
 terraform {
   required_providers {
-    intersight = {
-      source = "CiscoDevNet/intersight"
-      version = "1.0.0"
-    }
     thousandeyes = {
       source = "william20111/thousandeyes"
       version = "0.3.3"
     }
   }
-}
-
-provider "intersight" {
-  apikey = var.apikey
-  secretkey = var.secretkey
-  endpoint = "intersight.com"
-}
-
-provider "google" {
-  project     = var.project 
-  region      = var.region
 }
 
 provider "thousandeyes" {
--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
-variable "apikey" {
-  type    = string
-}
-
-variable "secretkey" {
-  type    = string
-}
-
-variable "k8s_version" {
-  type    = string
-  default = "1.18.12"
-}
-
 variable "te_token" {
   type    = string
 }
