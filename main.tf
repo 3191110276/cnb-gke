de78f58aae71c5445f769a63555f8ae294365005
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-03-25 10:47:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,11 @@ data "thousandeyes_agent" "test_agent" {
 }
 
 resource "thousandeyes_http_server" "google_http_test" {
+  test_name = "google test"
   interval = 120
   url = "https://google.com"
+  username = ""
+  password = ""
   agents {
       agent_id = data.thousandeyes_agent.test_agent.agent_id
   }
