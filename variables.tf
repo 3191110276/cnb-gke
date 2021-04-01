e2b829535597492435b40fd8f156b74aa2b055b0
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-01 15:00:02 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,19 +31,19 @@ variable "zone" {
 
 ### Optional parameters ###
 variable "cluster_service_account_name" {
-  description = "The name of the custom service account used for the GKE cluster. This parameter is limited to a maximum of 28 characters."
+  description = "The name of the custom service account used for the GKE cluster."
   type        = string
   default     = "example-cluster-sa"
 }
 
 variable "vpc_cidr_block" {
-  description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended."
   type        = string
   default     = "10.6.0.0/16"
 }
 
 variable "vpc_secondary_cidr_block" {
-  description = "The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  description = "The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended."
   type        = string
   default     = "10.7.0.0/16"
 }
