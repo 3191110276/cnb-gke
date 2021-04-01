ff2401ddf93af27e004b8defc069950285b6de5b
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-01 16:11:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,8 @@ module "google_container_cluster" {
   enable_vertical_pod_autoscaling = var.enable_vertical_pod_autoscaling
   #enable_workload_identity        = var.enable_workload_identity
   
-  #node_pool = var.node_pool
-  
-  #resource_labels = {
-  #  environment = "testing"
-  #}
+  node_pool = var.node_pool
+
 
 }
 
