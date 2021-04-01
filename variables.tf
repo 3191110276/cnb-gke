e50231a37b851c963613b8fe5f850c3fbbc72210
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-01 14:57:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+/*  
 variable "apikey" {
   type    = string
 }
@@ -10,7 +11,8 @@ variable "k8s_version" {
   type    = string
   default = "1.18.12"
 }
-
+ */
+ 
 variable "project" {
   description = "The project ID to host the cluster in."
   type = string
@@ -18,9 +20,52 @@ variable "project" {
 
 variable "region" {
   type = string
-  #default = "europe-west3"
+  default = "europe-west3"
 }
 
 variable "zone" {
   type = string
+}
+
+var "location" {
+  type = string
+}
+
+
+
+### Optional parameters ###
+variable "cluster_service_account_name" {
+  description = "The name of the custom service account used for the GKE cluster. This parameter is limited to a maximum of 28 characters."
+  type        = string
+  default     = "example-cluster-sa"
+}
+
+variable "vpc_cidr_block" {
+  description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  type        = string
+  default     = "10.6.0.0/16"
+}
+
+variable "vpc_secondary_cidr_block" {
+  description = "The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  type        = string
+  default     = "10.7.0.0/16"
+}
+
+variable "enable_vertical_pod_autoscaling" {
+  description = "Enable vertical pod autoscaling"
+  type        = string
+  default     = true
+}
+
+variable "enable_workload_identity" {
+  description = "Enable Workload Identity on the cluster"
+  default     = true
+  type        = bool
+}
+
+variable "override_default_node_pool_service_account" {
+  description = "When true, this will use the service account that is created for use with the default node pool that comes with all GKE clusters"
+  type        = bool
+  default     = false
 }
