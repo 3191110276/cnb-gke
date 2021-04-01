09c0fed344233376bc517da3814bbf8799bb3292
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-01 17:51:04 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,11 @@
 
 
 provider "google" {
-  version = "3.43.0"
   project = var.project
   region  = var.region
 }
 
 provider "google-beta" {
-  version = "3.43.0"
   project = var.project
   region  = var.region
 }
