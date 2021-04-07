241f35037a254e9d5162498988c8aae78427f379
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-07 12:20:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "intersight_organization_organization" "test" {
+  name = "default"
+}
+
 resource "intersight_kubernetes_sys_config_policy" "test123" {
 
   name = "test_k8s_sysconfig"
@@ -11,8 +15,4 @@ resource "intersight_kubernetes_sys_config_policy" "test123" {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.test.moid
   }
-}
-
-data "intersight_kubernetes_version" "version" {
-  kubernetes_version = join("", ["v", var.k8s_version])
 }
