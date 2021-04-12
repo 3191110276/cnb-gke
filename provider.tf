b88aa4d7a4d7418c6c41ee754dfcaa3bdfc4b55b
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-12 12:36:09 +0100

    Added AppD

--- provider.tf
+++ provider.tf
@@ -12,3 +12,22 @@ provider "intersight" {
   secretkey = var.secret_key
   endpoint = "intersight.com"
 }
+
+module "appdynamics" {
+  source  = "3191110276/appdynamics/kubernetes"
+  version = "0.1.5"
+  appd_account_name = var.appd_account_name
+  appd_controller_hostname = ""
+  appd_controller_key = ""
+  appd_controller_url = ""
+  appd_global_account = ""
+  appd_password = var.appd_pw
+  appd_username = var.appd_un
+  cluster_name = "default"
+  db_hostname = ""
+  db_name = ""
+  db_password = ""
+  db_port = ""
+  db_type = ""
+  db_username = ""
+}
