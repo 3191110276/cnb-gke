b3a9d5ae4f00d959c0eabe07e618cd2ce266732d
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-12 12:40:36 +0100

    Added AppD vars

--- variables.tf
+++ variables.tf
@@ -16,3 +16,16 @@ variable "k8s_version" {
 variable "te_token" {
   type    = string
 }
+
+# AppD 
+variable "appd_account_name" {
+  type    = string
+}
+
+variable "appd_pw" {
+  type    = string
+}
+
+variable "appd_un" {
+  type    = string
+}
