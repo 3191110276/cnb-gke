a55b8d3df8ca6ccf1139e81d6cb1cc747690affb
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-14 22:19:40 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,6 +13,6 @@ provider "kubernetes" {
 #provider "kubernetes" {
 #  load_config_file = false
 #  host = "https://${module.gke.endpoint}"
-#  token = module.google_client_config.default.access_token
+#  token = data.google_client_config.default.access_token
 #  cluster_ca_certificate = module.gke.ca_certificate
 #  }
