7b157cf0d92c670e45099043aaca4c7d52e2808d
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-15 15:54:34 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,14 +5,13 @@ provider "google" {
   region  = var.region
   }
 
-provider "kubernetes" {
-  source  = "hashicorp/kubernetes"
-  version = ">= 2.0.1"
-  }
-
 #provider "kubernetes" {
-#  load_config_file = false
-#  host = "https://${module.gke.endpoint}"
-#  token = data.google_client_config.default.access_token
-#  cluster_ca_certificate = module.gke.ca_certificate
+#  source  = "hashicorp/kubernetes"
+#  version = ">= 2.0.1"
 #  }
+
+provider "kubernetes" {
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  host                   = module.gke_auth.host
+  token                  = module.gke_auth.token
+  }
