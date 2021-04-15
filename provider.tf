b6b8315330f33379659051ef8851d3732653a68f
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-15 16:43:48 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,8 @@ provider "google" {
 #  }
 
 provider "kubernetes" {
+  load_config_file = "true"
+  config_path = local_file.kubeconfig.filename
   cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
   host                   = module.gke_auth.host
   token                  = module.gke_auth.token
