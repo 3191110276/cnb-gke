815a35b89f72ccad30abf04d8732457870479186
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-19 13:46:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+  
   required_version = ">= 0.12.26"
 }
 
