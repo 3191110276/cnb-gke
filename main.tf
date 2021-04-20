a0b631c13f1ddd47b822bbf486f1a04c05726fa9
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-20 15:25:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "gke-cisco" {
   source              = "namjachingu/gke-cisco/google"
-  version             = "0.1.10"
+  version             = "0.1.11"
   apikey              = var.apikey
   secretkey           = var.secretkey
   te_token            = var.te_token
