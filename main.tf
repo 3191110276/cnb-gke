bb309fddd692ddfafb023699ec4e7db0f0ff0e9a
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-23 10:42:17 +0100

    Code Update

--- main.tf
+++ main.tf
@@ -1,14 +1,34 @@
-data "thousandeyes_agent" "test_agent" {
-  name  = "na-sjc-2-te [VS01]"
-}
+resource "thousandeyes_page_load" "test2" {
+  test_name = "google_pageload"
+  
+  alerts_enabled         = 0
+  bandwidth_measurements = 0
+  network_measurements   = 0
 
-resource "thousandeyes_http_server" "google_http_test" {
-  test_name = "google test"
-  interval = 120
-  url = "https://google.com"
-  username = ""
-  password = ""
+  content_regex = ".*"
+  
+  url      = "https://google.com"
+  
+  interval      = 300
+  http_interval = 300
+	
   agents {
-      agent_id = data.thousandeyes_agent.test_agent.agent_id
+      agent_id = 144 # Vienna
+  }
+  
+  agents {
+      agent_id = 58 # Cairo
+  }
+  
+  agents {
+      agent_id = 60411 # Paris (Azure)
+  }
+	
+  agents {
+      agent_id = 56289 # Paris (AWS)
+  }
+	
+  agents {
+      agent_id = 65163 # Tokyo
   }
 }
--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     thousandeyes = {
       source = "william20111/thousandeyes"
-      version = "0.3.3"
+      version = "0.4.1"
     }
   }
 }
 
 provider "thousandeyes" {
-  token = "var.te_token"
+  token = var.token
 }
