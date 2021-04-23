a69d662fed8422a7ab79b717f78d2f7a34e7d346
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-23 12:41:50 +0100

    Update

--- main.tf
+++ main.tf
@@ -32,3 +32,22 @@ resource "thousandeyes_page_load" "test2" {
       agent_id = 65163 # Tokyo
   }
 }
+
+data "intersight_organization_organization" "test" {
+  name = "default"
+}
+
+resource "intersight_kubernetes_sys_config_policy" "test123" {
+
+  name = "test_k8s_sysconfig"
+
+  dns_servers = ["10.51.75.254"]
+
+  ntp_servers = ["10.51.75.240"]
+  timezone = "Europe/Vienna"
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.test.moid
+  }
+}
--- provider.tf
+++ provider.tf
@@ -11,3 +11,9 @@ terraform {
 provider "thousandeyes" {
   token = var.te_token
 }
+
+provider "intersight" {
+  apikey = var.api_key
+  secretkey = var.secret_key
+  endpoint = "intersight.com"
+}
