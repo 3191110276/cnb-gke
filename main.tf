61c3d53b3a38383332934bd57d36dbec659a58ca
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-23 14:11:47 +0100

    Update

--- main.tf
+++ main.tf
@@ -33,21 +33,21 @@ resource "thousandeyes_page_load" "test2" {
   }
 }
 
-# data "intersight_organization_organization" "test" {
-#   name = "default"
-# }
+data "intersight_organization_organization" "test" {
+  name = "default"
+}
 
-# resource "intersight_kubernetes_sys_config_policy" "test123" {
+resource "intersight_kubernetes_sys_config_policy" "test123" {
 
-#   name = "test_k8s_sysconfig"
+  name = "test_k8s_sysconfig"
 
-#   dns_servers = ["10.51.75.254"]
+  dns_servers = ["10.51.75.254"]
 
-#   ntp_servers = ["10.51.75.240"]
-#   timezone = "Europe/Vienna"
+  ntp_servers = ["10.51.75.240"]
+  timezone = "Europe/Vienna"
 
-#   organization {
-#     object_type = "organization.Organization"
-#     moid        = data.intersight_organization_organization.test.moid
-#   }
-# }
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.test.moid
+  }
+}
