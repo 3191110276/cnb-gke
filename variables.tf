5112dad18a5d9ac26b8275d27ace42a44927ddb6
elisemercury <56441652+elisemercury@users.noreply.github.com>
2021-04-23 11:06:14 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,31 @@
+# Intersight
+
+variable "api_key" {
+  type    = string
+}
+variable "secret_key" {
+  type    = string
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.18.12"
+}
+
+# ThousandEyes
 variable "te_token" {
+  type    = string
+}
+
+# AppD 
+variable "appd_account_name" {
+  type    = string
+}
+
+variable "appd_pw" {
+  type    = string
+}
+
+variable "appd_un" {
   type    = string
 }
